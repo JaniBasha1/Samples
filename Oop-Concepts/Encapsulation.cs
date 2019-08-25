3dc3eda5da7c0761d2d8b802192fa2fb6f424516
JaniBasha, Shaik <jani7basha86@gmail.com>
2019-08-25 15:27:56 +0530

    Encapsulation

---
+++ Oop-Concepts/Encapsulation.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Abstraction
+{
+   //Hiding the Implementation is Abstraction
+   //Console.WriteLine - We dont know what is behind it
+
+    // public are accessed outside of world
+    //public int a = 10, b=20;
+
+    //Encapulation within same class
+    private int a = 10, b=20;
+
+    //Encapsulation make availability to child classes
+    //protected int a = 10, b=20;
+    
+    //Only DoIt is visible to other world.
+    public void DoIt(){
+        Addition();
+    }
+
+    void Addition(){
+        Console.WriteLine("Abstraction Addition ...." + (a + b));
+    }
+}
--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Basic Hello World template generated in VS Code
 (i) Hiding the Implementation is Abstraction
 (ii) It is for re-usability, Provide Security
 (iii) Hide - Member, Method, Implementation
+
+Ex: In ArrayList we have different methods inbuild as Add,AddRange etc, we dont know their implement behind. We just know, what can we expect by using them. Other example could be Console.WriteLine
+
+3. Encapsulation
+
+(a) Binding the class members/ maintain relationship between class members
