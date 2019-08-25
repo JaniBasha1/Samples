3488b8f90640315d4eafd591eb916df8c1b5537e
JaniBasha, Shaik <jani7basha86@gmail.com>
2019-08-25 15:04:26 +0530

    oop-concept basic structure

---
+++ Oop-Concepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oop_Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
